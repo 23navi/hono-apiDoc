053deb4349eeeea8bdad87f6f1530d0ca0853bc0
23navi <navisureka23@gmail.com>
2025-04-05 12:07:42 +0530

    setup pino logger

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,28 @@
 import { OpenAPIHono  } from '@hono/zod-openapi'
 import { notFound, onError } from 'stoker/middlewares'
-import { pinoLogger } from './middlewares/pino-logger.js'
+import { pinoLogger } from '@/middlewares/pino-logger.js'
+import type { PinoLogger } from 'hono-pino'
 
-const app = new OpenAPIHono()
+
+type AppBindings = {
+    Variables: {
+        logger: PinoLogger
+    }
+}
+
+const app = new OpenAPIHono<AppBindings>()
 
 app.use(pinoLogger())
 app.notFound(notFound)
 app.onError(onError)
 
 app.get('/', (c) => {
+    c.var.logger.info("helloooo")
   return c.text('Hello Hono!')
 })
 
 app.get("/error",(c)=>{
     throw new Error("oooooo")
 })
-
-
-
-
 
 export default app
--- src/middlewares/pino-logger.ts
+++ src/middlewares/pino-logger.ts
@@ -1,5 +1,18 @@
-import {pinoLogger as logger} from "hono-pino"
+import env from "@/env.js";
+import { pinoLogger as logger } from "hono-pino";
+import {pino} from "pino";
+import pretty from "pino-pretty";
 
-export function pinoLogger(){
-    return logger()
+
+
+
+export function pinoLogger() {
+    return logger({
+        pino: pino({
+            level: env.LOG_LEVE
+        },env.NODE_ENV === "production" ? undefined: pretty()),
+        http:{
+            reqId:()=>crypto.randomUUID()
+        }
+    })
 }
